a78d1e2f7725a29f107236f5b07860e5eb4b5c43
lintommy85 <76210195+lintommy85@users.noreply.github.com>
2022-07-19 16:50:08 +0800

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+yum update -y
+echo -e "\033[31m-----yum update complete----- \033[0m"
+sleep 3
+yum -y install bzip2-devel  ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel -y
+echo -e "\033[31m-----python dependency package install complete----- \033[0m"
+sleep 3
+cd /opt
+wget https://www.python.org/ftp/python/3.7.13/Python-3.7.13.tar.xz
+echo -e "\033[31m-----python download complete----- \033[0m"
+sleep 3
+tar Jxvf Python-3.7.13.tar.xz && rm -rf Python-3.7.13.tar.xz
+echo -e "\033[31m-----python unzip & delete zip complete----- \033[0m"
+cd Python-3.7.13
+./configure --prefix=/opt/python
+echo -e "\033[31m-----python ./configure complete----- \033[0m"
+sleep 3
+make
+echo -e "\033[31m-----python make complete----- \033[0m"
+make install
+echo -e "\033[31m-----python make install complete----- \033[0m"
+sleep 3
+rm -rf /opt/Python-3.7.13/
+echo -e "\033[31m-----pythoninstall data delete----- \033[0m"

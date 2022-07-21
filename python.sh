91763a5e563d72143175e92fae48cad2c8d49830
林鈞庭 <lintommy85@gmail.com>
2022-07-21 12:04:24 +0800

    update ptyhon notice

--- python.sh
+++ python.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -e "\033[31m-----install python-3.9 yous openssl version minimum must be 1.1.1----- \033[0m"
+sleep 10
 yum update -y
 echo -e "\033[31m-----yum update complete----- \033[0m"
 sleep 3
@@ -7,12 +9,12 @@ yum -y install bzip2-devel  ncurses-devel sqlite-devel readline-devel tk-devel g
 echo -e "\033[31m-----python dependency package install complete----- \033[0m"
 sleep 3
 cd /opt
-wget https://www.python.org/ftp/python/3.7.13/Python-3.7.13.tar.xz
+wget https://www.python.org/ftp/python/3.9.13/Python-3.9.13.tar.xz
 echo -e "\033[31m-----python download complete----- \033[0m"
 sleep 3
-tar Jxvf Python-3.7.13.tar.xz && rm -rf Python-3.7.13.tar.xz
+tar Jxvf Python-3.9.13.tar.xz && rm -rf Python-3.9.13.tar.xz
 echo -e "\033[31m-----python unzip & delete zip complete----- \033[0m"
-cd Python-3.7.13
+cd Python-3.9.13
 ./configure --prefix=/opt/python
 echo -e "\033[31m-----python ./configure complete----- \033[0m"
 sleep 3
@@ -21,5 +23,6 @@ echo -e "\033[31m-----python make complete----- \033[0m"
 make install
 echo -e "\033[31m-----python make install complete----- \033[0m"
 sleep 3
-rm -rf /opt/Python-3.7.13/
+rm -rf /opt/Python-3.9.13/
 echo -e "\033[31m-----pythoninstall data delete----- \033[0m"
+echo -e "\033[31m-----$(pythin3 -V)----- \033[0m"
